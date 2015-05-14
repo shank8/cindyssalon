39069b07fc32ca432d80e0fd95d1a38962d37458
unknown <matt.hintzke@SKSEA-HINTZKE.skykick.com>
2015-05-13 23:30:21 -0700

    Updated

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get(/.*/, function(req, res) {
+  res.sendFile('index.html', {
+    root: __dirname + '/public'
+  });
+});
+
+app.get('/api/test', function(req, res){
+  res.send('hi');
 });
 
 app.listen(app.get('port'), function() {
